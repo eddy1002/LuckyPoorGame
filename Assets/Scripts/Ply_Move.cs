8d737d143611002b19346d94ba85bd4665bf359b
ChoYongHyeon <eddy1001@kookmin.ac.kr>
2018-03-02 17:10:50 +0900

    처음 기본 구성

---
+++ Assets/Scripts/Ply_Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ply_Move : MonoBehaviour {
+	public GameObject shootArea;
+	public GameObject bullets;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Attack() {
+		bullets.transform.position = shootArea.transform.position;
+		bullets.GetComponent<Rigidbody2D> ().velocity = new Vector2 (Random.Range(-10.0f, 10.0f), 10.0f);
+
+		gameObject.GetComponent<Animator> ().SetFloat ("Move", 0.0f);
+	}
+}
